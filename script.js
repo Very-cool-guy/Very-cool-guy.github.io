c52339b1ef4365669a14b38a019a99dcb4399a0c
random-nickname274 <whocouldvethought2846@gmail.com>
2026-05-22 10:24:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-var isDoomLoaded = false;
-console.doom = function() {
-  if (isDoomLoaded) return;
-  isDoomLoaded = true;
-  var script = document.createElement("script");
-  script.src = "https://console-doom.netlify.app/main.js";
-  document.head.appendChild(script);
-};
+
